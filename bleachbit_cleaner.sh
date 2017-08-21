884c7c0d8c90f8002fc263ebf1acaa90cc742c78
omariba <omariba5@gmail.com>
2017-08-21 18:12:50 +0300

    removed system.cache and system.memory

--- bleachbit_cleaner.sh
+++ bleachbit_cleaner.sh
@@ -1,1 +1,1 @@
-bleachbit -c google_chrome.cache google_chrome.cookies google_chrome.session firefox.cache firefox.cookie system.cache system.memory apt.clean
+bleachbit -c google_chrome.cache google_chrome.cookies google_chrome.session firefox.cache firefox.cookies apt.clean
